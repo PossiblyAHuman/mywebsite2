9e430bc69d4f10f30f310ed31d9b3514f39f4e56
Andy Zhou <azhou7@buffalo.edu>
2025-02-12 02:46:24 -0500

    dockerfile and compose added

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+ 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-  base: '/mywebsite2/',
-})
+  preview: {
+   port: 8080,
+   strictPort: true,
+  },
+  server: {
+   port: 8080,
+   strictPort: true,
+   host: true,
+   origin: "http://0.0.0.0:8080",
+  },
+ });
+
+/*
+export default defineConfig({
+  base: process.env.NODE_ENV === 'production' 
+        ? '/mywebsite-2/'  // Use the repo name for production (GitHub Pages)
+        : '/',   
+  plugins: [react()],
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http:0.0.0.0:8080",
+  },
+});*/
